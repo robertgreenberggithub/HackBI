33b60e6986c16cc2fb6dc5aa1357c5a64f3bd1c4
CatherineSalCode <catherine.salcas@gmail.com>
2018-11-03 21:31:13 -0400

    Quiz for the Ocean Safety

---
+++ Quiz.py
@@ -0,0 +1,32 @@
+print ("Welcome to the quiz! Take this before you read our html.")
+print ("Well, question 1. What is the biggest oxygen producer on the planet?")
+print ("A - trees. ")
+print ("B - factories")
+print ("C - plankton")
+print ("D - the ocean")
+print ("")
+
+Q1answer = "C"
+Q1response = raw_input("Q1answer : ")
+if (Q1response != Q1answer):
+   print ("incorrect")
+
+
+else:
+    print ("You have gotten the question correct")
+    print ("Question two: What is the main reason we should keep the oceans clean")
+    print ("1 - sweet fish")
+    print ("2 - biodiversity")
+    print ("3 - swimming")
+    print ("4 - great views")
+
+
+Q2answer = "2"
+Q2response = raw_input("Q1answer : ")
+if (Q2response != Q2answer):
+   print ("incorrect")
+
+
+else:
+    print ("You have completed the quiz")
+    
